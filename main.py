894e07fd62654f75ee47806573a9cb1ba33df5d3
combi_jihoon <>
2021-04-29 05:24:47 +0000

    The number of icecream : DFS, BFS completed

--- main.py
+++ main.py
@@ -7,16 +7,27 @@
 n, m = map(int, input().split())
 graph = []
 for _ in range(m) :
-	graph.append(list(map(int, input().split())))
+	graph.append(list(map(int, input())))
 
 result = 0
 
 def dfs(x, y) :
-	global graph
+	if x < 0 or x >= n or y < 0 or y >= m :
+		return False
 
 	if graph[x][y] == 0 :
 		graph[x][y] = 1
-		dfs(x-1, y)
+		dfs(x-1, y) 
+		dfs(x, y+1)
 		dfs(x+1, y)
 		dfs(x, y-1)
-		dfs(x, y+1)
+
+		return True
+	return False
+
+for i in range(n) :
+	for j in range(m) :
+		if dfs(i, j) == True :
+			result += 1
+
+print(result)
