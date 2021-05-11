56e40ae36d842e73b770c466ac9ac6f0b98fdd0b
combi_jihoon <combi_jihoon@naver.com>
2021-05-11 13:18:46 +0900

    Complete unsolved problems for some test cases

--- Kakao_2021_Recommendation_for_new_ids.py
+++ Kakao_2021_Recommendation_for_new_ids.py
@@ -2,47 +2,27 @@ def solution(new_id):
     temp1 = ''
     new_id = list(new_id)
 
-    # 제거하려는 것은 "#"로 치환한다.
     acceptableList = ['-', '_', '.']
-    for i in range(1, 9) :
-        acceptableList.append(str(i))
-    lowerAlpha = [chr(65+i) for i in range(26)]
-    upperAlpha = [chr(97+i) for i in range(26)]
-    Alpha = lowerAlpha + upperAlpha
     for id in new_id :
-        if id in Alpha :
+        if id.isalpha() :
             temp1 += id.lower()
-        elif id in acceptableList :
+        elif id.isdigit() or id in acceptableList :
             temp1 += id
-    temp1 = list(temp1)
-    # 연속된 마침표 처리
-    stickIndex = 0
-    while stickIndex + 1 < len(temp1) :
-        if temp1[stickIndex] == '.' and temp1[stickIndex+1] == '.' :
-            del temp1[stickIndex]
-        else :
-            stickIndex += 1
-    temp1 = "".join(temp1)
+        
+    while '..' in temp1 :
+        temp1 = temp1.replace('..', '.')
 
+                
     # 처음과 끝 마침표 처리
-    i = 0
-    j = 1
-    while True :
-        if len(temp1) == 1 and temp1 == "." :
-            temp1 = ""
-            break
-        if len(temp1) >= 2 :
-            if temp1[0] == '.'  :
-                temp1 = temp1[1:]
-            elif temp1[-1] == '.' :
-                temp1 = temp1[:-1]
-            else :
-                break
+    if temp1[0] == '.' : 
+        temp1 = temp1[1:] 
+    if temp1[-1] == '.' :
+        temp1 = temp1[:-1] 
 
     # 빈 문자열 처리       
     if temp1 == "" :
         temp1 += "a"
-        
+     
     # 길이 수 조절
     if len(temp1) >= 16 :
         temp1 = temp1[:15]
@@ -52,5 +32,6 @@ def solution(new_id):
     if len(temp1) <= 2 :
         while len(temp1) != 3 :
             temp1 += temp1[-1]
-    
     return temp1
+
+print(solution("abcdefghijklmn.p"))
